69b0ef35876d429df6c428201ee822e7b0153308
Chow529 <1165969446@qq.com>
2026-07-29 16:10:13 +0800

    修改目录结构2.0

--- utils/path_tool.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def get_projiect_root() -> str:
-    curent_file = os.path.abspath(__file__)
-    curent_dir = os.path.dirname(curent_file)
-    return os.path.dirname(curent_dir)
-
-def get_abs_path(filePath:str) ->str :
-    return os.path.join(get_projiect_root(),filePath) 
